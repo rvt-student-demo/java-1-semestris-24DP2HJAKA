3a544fc3e6d6cc10cba0146f871c3274a610979f
hardijs <a242592hj@rvt.lv>
2025-09-10 13:07:31 +0300

    various_vriables

---
+++ src/main/java/rvt/various_variables.java
@@ -0,0 +1,23 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class various_variables {
+
+    public static void main(String[] args) {
+        int chicken = 3;
+        double bacon_kg = 5.5;
+        String tractor = "None";
+        int summary = 3;
+        System.out.println("Chicken: ");
+        System.out.println(chicken);
+        System.out.println("bacon (kg):");
+        System.out.println(bacon_kg);
+        System.out.println("tractor:");
+        System.out.println(tractor);
+        System.out.println("And the summary");
+        System.out.println(summary);
+
+            
+    }
+}
