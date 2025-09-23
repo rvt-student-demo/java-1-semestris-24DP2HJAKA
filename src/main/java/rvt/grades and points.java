25153fa8dc03d57fa816cc09a8820e388a2f0c20
jakadels <a242592hj@rvt.lv>
2025-09-23 09:49:02 +0300

    grades and points

---
+++ src/main/java/rvt/grades and points.java
@@ -0,0 +1,24 @@
+package rvt;
+
+public class grades and points {
+    
+}
+public static String getGrade(int points) {
+    if (points < 0) {
+        return "impossible!";
+    } else if (points <= 49) {
+        return "failed";
+    } else if (points <= 59) {
+        return "1";
+    } else if (points <= 69) {
+        return "2";
+    } else if (points <= 79) {
+        return "3";
+    } else if (points <= 89) {
+        return "4";
+    } else if (points <= 100) {
+        return "5";
+    } else {
+        return "incredible!";
+    }
+}
---
+++ src/main/java/rvt/reizinashana.java
@@ -0,0 +1,11 @@
+public class reizinashana {
+	public static void main(String[] args) {
+		int first = 2;
+		System.out.println(first); // prints 2
+		int second = 4;
+		System.out.println(second); // prints 4
+
+		int sum = first + second; // The sum of the values of the variables first and second is assigned to the variable sum
+		System.out.println(sum); // prints 6
+	}
+}
