04708bb6b3c7d1c5bf14e25a234ee86c15aeb633
jakadels <a242592hj@rvt.lv>
2025-10-28 11:05:34 +0200

    klases stunda_stastictics

---
+++ src/main/java/rvt/Main.java
@@ -0,0 +1,33 @@
+package rvt;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Statistics allNumbers = new Statistics();
+        Statistics evenNumbers = new Statistics();
+        Statistics oddNumbers = new Statistics();
+
+        System.out.println("Enter numbers:");
+        while (true) {
+            int number = Integer.parseInt(scanner.nextLine());
+            if (number == -1) {
+                break;
+            }
+
+            allNumbers.addNumber(number);
+
+            if (number % 2 == 0) {
+                evenNumbers.addNumber(number);
+            } else {
+                oddNumbers.addNumber(number);
+            }
+        }
+
+        System.out.println("Sum: " + allNumbers.sum());
+        System.out.println("Sum of even numbers: " + evenNumbers.sum());
+        System.out.println("Sum of odd numbers: " + oddNumbers.sum());
+    }
+}
