9481c984594b117e8a34b886aa7dae6aeaf7425f
jakadels <a242592hj@rvt.lv>
2025-10-31 09:06:01 +0200

    saku paymentcard

---
+++ src/main/java/rvt/PaymentCard.java
@@ -0,0 +1,11 @@
+public class PaymentCard {
+    private double balance;
+
+    public PaymentCard(double openingBalance) {
+        // write code here
+    }
+
+    public String toString() {
+        // write code here
+    }
+}
