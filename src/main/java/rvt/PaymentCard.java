fdfb45d14c3006c8f76f028d4a822752ddcabe91
jakadels <a242592hj@rvt.lv>
2025-12-09 17:18:54 +0000

    java mooc 4.1

--- src/main/java/rvt/PaymentCard.java
+++ src/main/java/rvt/PaymentCard.java
@@ -1,11 +1,31 @@
+package rvt;
+
 public class PaymentCard {
     private double balance;
 
     public PaymentCard(double openingBalance) {
-        // write code here
+        this.balance = openingBalance;
     }
-
     public String toString() {
-        // write code here
+        return "Sitai kartei balance ir " + this.balance + " Euro";
+    }
+    public void eatAffordibly() {
+        if(this.balance > 2.60) {
+        this.balance = this.balance - 2.60;
+        }
+    }
+    public void eatHeartily() {
+        if(this.balance > 4.60) {
+        this.balance = this.balance - 4.60;
+        }
+    }
+    public void AddMoney (double amount) {
+    
+        if(amount > 0) {
+            this.balance = this.balance + amount;
+            if(this.balance >= 150) {
+                this.balance = 150;
+            }
+        }
     }
 }
